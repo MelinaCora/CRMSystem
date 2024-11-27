f10d76c54c1cae315fdc19b1106e25a456ddcb20
MelinaCora <melinaccora97@gmail.com>
2024-11-27 10:24:07 -0300

    Se modifico la valiacion del dia de inicio del proyecto ya que tomaba la hora y no dejaba crear proyectos en el dia de la fecha

--- Aplication/Services/ProjectService.cs
+++ Aplication/Services/ProjectService.cs
@@ -56,7 +56,7 @@ namespace Aplication.Services
                 throw new RequiredParameterException("Error. Start date is required");
             }
 
-            if (request.start < DateTime.Now)
+            if (request.start.Date < DateTime.Now.Date)
             {
                 throw new RequiredParameterException("Error. Start date cannot be in the past");
             }
